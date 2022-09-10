4bd74ffc51f1cfd239181dd5eccbd10142da5801
devUgur <ugur44ist@gmail.com>
2022-09-10 19:23:55 +0200

    Themplate 01

--- themplate01/src/router/index.js
+++ themplate01/src/router/index.js
@@ -1,11 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+
+import LandingPageView from "@/views/landing-page/LandingPage.view";
 
 const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: LandingPageView
   },
   {
     path: '/about',
---
+++ themplate01/src/views/landing-page/LandingPage.view.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="landing-page">
+    <img src="@/assets/instagram/07.jpg">
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+#landing-page{
+  min-height: 100vh;
+}
+</style>
